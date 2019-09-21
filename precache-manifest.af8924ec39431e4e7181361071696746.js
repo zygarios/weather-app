2d2d3bcf2321663f02e05326aed8bca891aac354
Bartosz Zygmunt <bartekzygmunt92@gmail.com>
2019-09-21 12:56:03 +0200

    Updates

---
+++ precache-manifest.af8924ec39431e4e7181361071696746.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fc8010f7d3ecbc252fb0314ebbca502",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "07807237316b6bb9b5fa",
+    "url": "/weather-app/static/css/main.8a8dc552.chunk.css"
+  },
+  {
+    "revision": "dc5efbde975c1a31198e",
+    "url": "/weather-app/static/js/2.56991a9e.chunk.js"
+  },
+  {
+    "revision": "07807237316b6bb9b5fa",
+    "url": "/weather-app/static/js/main.2d5f43b6.chunk.js"
+  },
+  {
+    "revision": "41f196e7b115d4760035",
+    "url": "/weather-app/static/js/runtime~main.64c1ba9b.js"
+  },
+  {
+    "revision": "aba77d5cd21c75b3b2462c9cc396fab3",
+    "url": "/weather-app/static/media/bright-cloud.aba77d5c.png"
+  },
+  {
+    "revision": "f57e323e88230a82ae577f9a8141d272",
+    "url": "/weather-app/static/media/dark-cloud.f57e323e.png"
+  },
+  {
+    "revision": "ee115c398257e1a9fe52cfd0dccd6c0c",
+    "url": "/weather-app/static/media/dryland.ee115c39.png"
+  },
+  {
+    "revision": "59f9050c68eb0b10fc0c30e379f23428",
+    "url": "/weather-app/static/media/grass.59f9050c.png"
+  },
+  {
+    "revision": "4c9bb2c5bbde028cba217866637f3827",
+    "url": "/weather-app/static/media/grass_snow.4c9bb2c5.png"
+  },
+  {
+    "revision": "46089f71788d6323354ab8c40d40feee",
+    "url": "/weather-app/static/media/grassland-snow.46089f71.png"
+  },
+  {
+    "revision": "b93b11ad4168c8a47b9c22d60fb0b5f4",
+    "url": "/weather-app/static/media/grassland.b93b11ad.png"
+  },
+  {
+    "revision": "32a93da921602cf49ddc0f20bf77d8aa",
+    "url": "/weather-app/static/media/heavy-rain.32a93da9.png"
+  },
+  {
+    "revision": "2b1cec836a34c6c796fc2c207b9553ff",
+    "url": "/weather-app/static/media/lighting.2b1cec83.png"
+  },
+  {
+    "revision": "d4056aa2cb67d5a5e0c678cdddc14464",
+    "url": "/weather-app/static/media/mist.d4056aa2.png"
+  },
+  {
+    "revision": "3e23f13eae9a94bd102d300a64bd87e0",
+    "url": "/weather-app/static/media/moon.3e23f13e.png"
+  },
+  {
+    "revision": "b29b17d135e3f065ce39865da6dfb130",
+    "url": "/weather-app/static/media/planet.b29b17d1.png"
+  },
+  {
+    "revision": "288f799afd26d3e7826c0bc5d25ba25c",
+    "url": "/weather-app/static/media/rain.288f799a.png"
+  },
+  {
+    "revision": "41e22a77a9bf62c4f2bb7d80d2bfe354",
+    "url": "/weather-app/static/media/snow.41e22a77.png"
+  },
+  {
+    "revision": "453986736f1b945a89f292be3d55288f",
+    "url": "/weather-app/static/media/stars.45398673.png"
+  },
+  {
+    "revision": "87d2ee6934fb82307d654755542a63ee",
+    "url": "/weather-app/static/media/sun.87d2ee69.png"
+  },
+  {
+    "revision": "b766aea652652428261e1442563b352c",
+    "url": "/weather-app/static/media/tumbleweed.b766aea6.png"
+  }
+]);
